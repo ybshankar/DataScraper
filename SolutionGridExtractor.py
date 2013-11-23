8ab6fbfe7ea45756386491c375659e0fb722d2c6
byanamadala <byanamadala@BHAVANI-990.fngn.com>
2013-11-22 16:23:10 -0800

    Added Logic to Parse the Puzzle and print out the grid using the Puzzle Image. Added independent logic to number cells (not using OCR)

--- SolutionGridExtractor.py
+++ SolutionGridExtractor.py
@@ -22,11 +22,11 @@ def parseSolutionImage(solutionImage):
     squareSize = int(5*x/15)
     processedImage = processedImage.resize((int(x*5), int(y*5)), Image.BICUBIC)
     #processedImage = ImageEnhance.Contrast(processedImage)
-    processedImageImg = processedImage.convert("RGBA")
-    pix = processedImageImg.load()
+    processedImage = processedImage.convert("RGBA")
+    pix = processedImage.load()
     
-    for y in xrange(processedImageImg.size[1]):
-        for x in xrange(processedImageImg.size[0]):
+    for y in xrange(processedImage.size[1]):
+        for x in xrange(processedImage.size[0]):
             borderPixel = ((x % squareSize) < 15) or \
                           ((x % squareSize) > (squareSize - 15)) or \
                           ((y % squareSize) < 15) or \
@@ -39,42 +39,42 @@ def parseSolutionImage(solutionImage):
                 pix[x, y] = (255, 255, 255, 255)
     
     #processedImage = processedImage.convert()
-    processedImage = processedImageImg.convert('L')
+    processedImage = processedImage.convert('L')
     processedImage = processedImage.resize((x, y), Image.NEAREST)
-    filteredImage = filterAndReturnImage(processedImage)
-    solutionString = "Default filters : (minFilter(7), blur) \n\n"
-    solutionString =  solutionString + image_to_string(filteredImage, True) +'\n\n'
-    filteredImage = filterAndReturnImage(processedImage, minFilter=3, maxFilter=3, blur=True)
-    solutionString = solutionString + "Filters : (minFilter(3), blur) \n\n"
-    solutionString =  solutionString + image_to_string(filteredImage, True) + '\n\n'
+    
     filteredImage = filterAndReturnImage(processedImage, minFilter=9, maxFilter=0, blur=True)
-    solutionString = solutionString + "Filters : (minFilter(5), blur) \n\n"
-    solutionString =  solutionString + image_to_string(filteredImage, True) + '\n\n'
-    filteredImage = filterAndReturnImage(processedImage, minFilter=5, maxFilter=9, blur=True)
-    solutionString = solutionString + "Filters : (minFilter(9), blur) \n\n"
-    solutionString =  solutionString + image_to_string(filteredImage, True) + '\n\n'
-    filteredImage = filterAndReturnImage(processedImage, minFilter=11, maxFilter=21, blur=True)
-    solutionString = solutionString + "Filters : (minFilter(21), blur) \n\n"
-    solutionString =  solutionString + image_to_string(filteredImage, True) + '\n\n'
-    filteredImage = filterAndReturnImage(processedImage, minFilter=15, maxFilter=7, blur=False)
-    solutionString = solutionString + "Filters : (minFilter(7)) \n\n"
+    filteredImage.show("30% more contrast")
+    solutionString = "Filters : minFilter=9, maxFilter=0, blur=True \n\n"
     solutionString =  solutionString + image_to_string(filteredImage, True) +'\n\n'
-    filteredImage = filterAndReturnImage(processedImage, minFilter=9, maxFilter=3, blur=False)
-    solutionString = solutionString + "Filters : (minFilter(3)) \n\n"
+
+    filteredImage = filterAndReturnImage(processedImage, minFilter=9, maxFilter=0, blur=False)
+    filteredImage.show("30% more contrast")
+    solutionString = solutionString +  "Filters : minFilter=9, maxFilter=0, blur=False \n\n"
+    solutionString =  solutionString + image_to_string(filteredImage, True) +'\n\n'
+    
+    filteredImage = filterAndReturnImage(processedImage, minFilter=7, maxFilter=0, blur=True)
+    filteredImage.show("30% more contrast")
+    solutionString = solutionString + "Filters : minFilter=7, maxFilter=0, blur=True \n\n"
     solutionString =  solutionString + image_to_string(filteredImage, True) + '\n\n'
-    filteredImage = filterAndReturnImage(processedImage, minFilter=3, maxFilter=5, blur=False)
-    solutionString = solutionString + "Filters : (minFilter(5)) \n\n"
+
+    filteredImage = filterAndReturnImage(processedImage, minFilter=7, maxFilter=0, blur=False)
+    filteredImage.show("30% more contrast")
+    solutionString = solutionString + "Filters : minFilter=7, maxFilter=0, blur=False \n\n"
     solutionString =  solutionString + image_to_string(filteredImage, True) + '\n\n'
-    filteredImage = filterAndReturnImage(processedImage, minFilter=3, maxFilter=9, blur=False)
-    solutionString = solutionString + "Filters : (minFilter(9), blur) \n\n"
+    
+    filteredImage = filterAndReturnImage(processedImage, minFilter=11, maxFilter=0, blur=True)
+    filteredImage.show("30% more contrast")
+    solutionString = solutionString + "Filters : minFilter=11, maxFilter=0, blur=True \n\n"
+    solutionString =  solutionString + image_to_string(filteredImage, True) + '\n\n'
+    
+    filteredImage = filterAndReturnImage(processedImage, minFilter=11, maxFilter=0, blur=False)
+    filteredImage.show("30% more contrast")
+    solutionString = solutionString + "Filters : minFilter=11, maxFilter=0, blur=False \n\n"
     solutionString =  solutionString + image_to_string(filteredImage, True) + '\n\n'
 
     return solutionString
 
-
-
-
-def filterAndReturnImage(inImage,minFilter=9, maxFilter=7, blur=True):
+def filterAndReturnImage(inImage,minFilter=9, maxFilter=0, blur=True):
     if minFilter > 0:
         outImage = inImage.filter(ImageFilter.MinFilter(minFilter))
         
@@ -86,15 +86,163 @@ def filterAndReturnImage(inImage,minFilter=9, maxFilter=7, blur=True):
         
     return outImage
 
+def parsePuzzleImage(puzzleImage):
+    puzzleString = ""
+    processedImage = trimBorders(puzzleImage)
+    x,y = processedImage.size
+    numCells = 15
+    cellSize = int(5*x/numCells)
 
-def createPuzzleString(puzzleImage):
-    puzzleString = None
+    processedImage = processedImage.resize((int(x*5), int(y*5)), Image.BICUBIC)
+    processedImage = processedImage.convert("RGBA")
+    
+    processedImage = processedImage.filter(ImageFilter.BLUR)
+    pix = processedImage.load()
+    
+    for y in xrange(processedImage.size[1]):
+        for x in xrange(processedImage.size[0]):
+            if pix[x, y] <=  (230, 230, 230, 255):
+                pix[x, y] = (0, 0, 0, 255)
+            else:
+                pix[x, y] = (255, 255, 255, 255)
+    
+    processedImage = processedImage.convert('L')
+    processedImage = processedImage.resize((x, y), Image.NEAREST)
+    filteredImage = processedImage
+    #filteredImage = filterAndReturnImage(processedImage, minFilter=15, maxFilter=3, blur=True)
+
+    #filteredImage = processedImage.filter(ImageFilter.MaxFilter(15))
+    #filteredImage = filteredImage.filter(ImageFilter.MinFilter(7))
+    #filteredImage = filteredImage.filter(ImageFilter.ModeFilter(21))
+    #filteredImage.show("30% more contrast")
+    
+    for cellY in xrange(numCells):
+        puzzleString = puzzleString + "\n"
+        for cellX in xrange(numCells):
+            startX = int(cellX*cellSize)
+            startY = int(cellY*cellSize)
+            box = (startX, startY , startX + cellSize, startY+cellSize)
+            cellImage = filteredImage.crop(box)
+            #cellImage.show("")
+            colors = cellImage.getcolors()
+            pixels = cellImage.size[0]*cellImage.size[1]
+            
+            for color in colors:
+                if color[0]> int(pixels/2):
+                    dominantColor = color
+                    break
+
+            if dominantColor[1] == 0:
+                cellColor = 'X'
+            elif dominantColor[1] == 255:
+                cellColor = '_'
+
+            puzzleString = puzzleString + cellColor
+
     return puzzleString
 
+def displayPuzzleString(puzzleStr):
+    displayPuzzleStr = puzzleStr.replace('X', ' X ')
+    displayPuzzleStr = displayPuzzleStr.replace('_', ' __')
+    return displayPuzzleStr
+
+
+def enhancedPuzzleString(puzzleStr):
+    
+    puzzleGrid = puzzleStr.split('\n')
+    puzzleGrid = [x for x in puzzleGrid if x not in ['\n', '']]
+    enhPuzzleGrid = ["" for x in xrange(len(puzzleGrid))]
+    clueCount = 1
+    for i in xrange(len(puzzleGrid)):
+        for j in xrange(len(puzzleGrid[0])):
+            if puzzleGrid[i][j] == "X":
+                enhPuzzleGrid[i] = enhPuzzleGrid[i] + " X "
+            else:
+                neighbours = {'North' : False, 
+                              'South' : False, 
+                              'East' : False,
+                              'West' : False}
+                
+                if (i == 0): 
+                    neighbours['North'] =  True
+                    if (puzzleGrid[i+1][j] == "X"):
+                        neighbours['South'] = True
+                elif (i==(len(puzzleGrid)-1)): 
+                    neighbours['South'] =  True
+                    if (puzzleGrid[i-1][j] == "X"):
+                        neighbours['North'] = True
+                else:
+                    if (puzzleGrid[i-1][j] == "X"):
+                        neighbours['North'] =  True
+                    if (puzzleGrid[i+1][j] == "X"):
+                        neighbours['South'] =  True
+                
+                if (j == 0): 
+                    neighbours['East'] =  True
+                    if (puzzleGrid[i][j+1] == "X"):
+                        neighbours['West'] =  True
+                elif (j==(len(puzzleGrid)-1)): 
+                    neighbours['West'] =  True
+                    if (puzzleGrid[i][j-1] == "X"):
+                        neighbours['East'] =  True
+                else:
+                    if (puzzleGrid[i][j-1] == "X"):
+                        neighbours['East'] =  True
+                    if (puzzleGrid[i][j+1] == "X"):
+                        neighbours['West'] =  True
+                
+                northAndSouthAreSame = (neighbours['North'] and neighbours['South']) or \
+                                      ((not neighbours['North']) and (not neighbours['South']))
+                
+                eastAndWestAreSame = (neighbours['East'] and neighbours['West']) or \
+                                      ((not neighbours['East']) and (not neighbours['West']))
+                                      
+                newClue = False
+                
+                if  northAndSouthAreSame:
+                    if neighbours['East'] and (not neighbours['West']):
+                        newClue = True
+                elif eastAndWestAreSame:
+                    if neighbours['North'] and (not neighbours['South']):
+                        newClue = True
+                else:
+                    if not (neighbours['South'] and neighbours['West']):
+                        newClue=True
+                
+                if newClue:
+                    if clueCount <= 9:
+                        enhPuzzleGrid[i] = enhPuzzleGrid[i] + str(clueCount) + "__"
+                    else:
+                        enhPuzzleGrid[i] = enhPuzzleGrid[i] + str(clueCount) + "_"
+                    clueCount = clueCount+1
+                else:
+                    enhPuzzleGrid[i] = enhPuzzleGrid[i] + " __"
+
+    enhPuzzleGrid = "\n".join(enhPuzzleGrid)
+    return enhPuzzleGrid
+
+    
+
+def test_solutionParsing(puzzleNum=10875):
+     solution = Image.open("puzzles/solution/"+ str(puzzleNum)+".jpg")
+     print parseSolutionImage(solution)
+
+def test_puzzleParsing(puzzleNum=10875):
+    puzzle = Image.open("puzzles/puzzle/"+ str(puzzleNum)+".jpg")
+    puzzleStr = parsePuzzleImage(puzzle)
+    print puzzleStr
+    print displayPuzzleString(puzzleStr)
+    print ""
+    print enhancedPuzzleString(puzzleStr)
+
 if  __name__ =='__main__':
-    puzzle_num = 10875
-    solution = Image.open("puzzles/solution/"+ str(puzzle_num)+".jpg")
-    print parseSolutionImage(solution)
+    test_puzzleParsing()
+#    puzzleNum = 10875
+#     solution = Image.open("puzzles/solution/"+ str(puzzleNum)+".jpg")
+#     print parseSolutionImage(solution)
+
+#    puzzle = Image.open("puzzles/puzzle/"+ str(puzzleNum)+".jpg")
+#    print parsePuzzleImage(puzzle)
 # def get(link):
 #     urlretrieve(link,'16TH_CROSS_SOL__12_1616119g.jpg')
  
